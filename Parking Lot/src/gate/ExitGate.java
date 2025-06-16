4bbf3312909f053faa232dff407a0bad0957aa8c
Abhijit <abhijitmudoi@gmail.com>
2025-06-17 01:18:18 +0530

    implementing the main class

--- Parking Lot/src/gate/ExitGate.java
+++ Parking Lot/src/gate/ExitGate.java
@@ -1,5 +1,6 @@
 package gate;
 
+import model.Vehicle;
 import parkingLot.ParkingLot;
 import parkingLot.parkingSpot.ParkingSpot;
 import payment.PaymentService;
@@ -16,15 +17,26 @@ public class ExitGate {
     public void processExit(int spotNumber, int hoursStayed) {
         // find the spot by number
         ParkingSpot spot = parkingLot.getSpotByNumber(spotNumber);
-        if(spot == null || !spot.isOccupied()) {
+        if (spot == null || !spot.isOccupied()) {
             System.out.println("Invalid or vacant spot!");
             return;
         }
 
         // get the vehicle in the spot
-        
+        Vehicle vehicle = spot.getVehicle();
+        if (vehicle == null) {
+            System.out.println("No vehicle found in the given spot!");
+            return;
+        }
+
+        // calculate the parking fee
+        double fee = vehicle.calculateFee(hoursStayed);
+
+        // delegate the payment processing to payment service
+        paymentService.makePayment(fee);
+
+        // vacant the spot after payment
+        parkingLot.vacateSpot(spot, vehicle);
+        System.out.println("Spot vacated successfully!");
     }
-
-
-
 }
